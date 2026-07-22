2592bf51a72a8d06430927b51d34a4f95694e075
TAPSOBA Ghislain <arseneghislaintaps@gmail.com>
2026-07-22 23:14:58 +0000

    "Ajout configuration Terraform (provider Docker) + gitignore"

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_network" "emploi_du_temps_network" {
+  name = "emploi-du-temps-network"
+}
+
+resource "docker_volume" "pgdata" {
+  name = "emploi-du-temps-pgdata"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:16"
+}
+
+resource "docker_container" "postgres" {
+  name  = "pg-emploi-du-temps"
+  image = docker_image.postgres.image_id
+
+  networks_advanced {
+    name = docker_network.emploi_du_temps_network.name
+  }
+
+  env = [
+    "POSTGRES_USER=postgres",
+    "POSTGRES_PASSWORD=postgres",
+    "POSTGRES_DB=emploi_du_temps"
+  ]
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+
+  volumes {
+    volume_name    = docker_volume.pgdata.name
+    container_path = "/var/lib/postgresql/data"
+  }
+}
+
+resource "docker_image" "app" {
+  name         = "emploi-du-temps-app:terraform"
+  keep_locally = true
+}
+
+resource "docker_container" "app" {
+  name  = "emploi-du-temps-app-tf"
+  image = docker_image.app.image_id
+
+  networks_advanced {
+    name = docker_network.emploi_du_temps_network.name
+  }
+
+  env = [
+    "SPRING_DATASOURCE_URL=jdbc:postgresql://pg-emploi-du-temps:5432/emploi_du_temps",
+    "SPRING_DATASOURCE_USERNAME=postgres",
+    "SPRING_DATASOURCE_PASSWORD=postgres"
+  ]
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+
+  depends_on = [docker_container.postgres]
+}
